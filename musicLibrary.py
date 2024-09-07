00ec32e75743296dd1b6fd51b1200ca2532faae7
utology <124037892+dev-ploy@users.noreply.github.com>
2024-09-08 01:15:06 +0530

    This is about audio files used in the chatbot

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music={
+    "sleath":"https://www.youtube.com/watch?v=U47Tr9BB_wE",
+    "good":"https://www.youtube.com/watch?v=uRJQJcy3f8w",
+    "skyfall":"https://www.youtube.com/watch?v=DeumyOzKqgI",
+    "bgm":"https://www.youtube.com/watch?v=TrRTV3Yac50"
+}
